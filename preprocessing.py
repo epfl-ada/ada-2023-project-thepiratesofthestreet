581b38e77519dcf44bbd7fa8d53a7a531ec6b9cf
G.Soldati <geza.soldati@epfl.ch>
2023-12-15 14:45:52 +0100

    Clean dataset from imdb and cmu

--- preprocessing.py
+++ preprocessing.py
@@ -52,8 +52,60 @@ def get_movie_genres_dataframe(df_movies):
     print(df_genres.head())
     return df_genres
 
+def get_unique_genres_imdb_dataframe(df):
+    uniques = []
+    for i in df['genres']:
+        if not isinstance(i, str):
+            print(i)
+            continue
+        elements = i.replace(' ','').split(',')
+        for element in elements:
+            if not element in uniques:
+                uniques.append(element)
+    #['Documentary',
+    #  'Short',
+    #  'Animation',
+    #  'Comedy',
+    #  'Romance',
+    #  'Sport',
+    #  'News',
+    #  'Drama',
+    #  'Fantasy',
+    #  'Horror',
+    #  'Biography',
+    #  'Music',
+    #  'War',
+    #  'Crime',
+    #  'Western',
+    #  'Family',
+    #  'Adventure',
+    #  'Action',
+    #  'History',
+    #  'Mystery',
+    #  '\\N',
+    #  'Sci-Fi',
+    #  'Musical',
+    #  'Thriller',
+    #  'Film-Noir',
+    #  'Talk-Show',
+    #  'Game-Show',
+    #  'Reality-TV',
+    #  'Adult']
+    return uniques 
+
+def get_fictional_movies_idmb_dataframe(df_movies, df_imdb_title_genre):
+
+    df_genred = df_imdb_title_genre[~df_imdb_title_genre['genres'].isna()]
+    selected_rows = df_genred[df_genred['genres'].str.contains('Sci-Fi|Fantasy', case=False, regex=True)]
+    df_fictional_imdb = pd.merge(df_movies,
+                                 selected_rows[[ 'tconst', 'titleType', 'isAdult', 'primaryTitle', 'originalTitle', 'startYear', 'genres']],
+                                   left_on='movie_name', right_on='primaryTitle', how='inner')
+    return df_fictional_imdb
+
+
 def get_fictional_summaries_subset(df_genres, df_movies, df_summaries,
                                 fictional_genres =  ['Science Fiction', 'Fantasy'],
+                                return_df_fictional = False,
                                 verbose = True):
     '''
     Gets subset of summaries with fictional genres
@@ -67,7 +119,10 @@ def get_fictional_summaries_subset(df_genres, df_movies, df_summaries,
     fictional_wiki_movie_id = df_fictional['Wikipedia_movie_ID'].unique().copy() #defined in Part 1 ('Science Fiction' and 'Fantasy')
     df_fictional_summaries = df_summaries[df_summaries['Wikipedia_movie_ID'].isin(fictional_wiki_movie_id)]
     
-    return df_fictional_summaries
+    if return_df_fictional:
+        return df_fictional_summaries, df_movies[df_movies['Wikipedia_movie_ID'].isin(df_fictional['Wikipedia_movie_ID'])]
+    else:
+        return df_fictional_summaries
     
 
 def filter_names(text, out_token = False):
